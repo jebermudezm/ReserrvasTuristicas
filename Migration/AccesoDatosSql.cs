034b7cdd635a747e826fe983f5dfa7e6963c842d
Joaquin Bermudez <jeberme@hotmail.com>
2020-09-14 19:17:26 -0500

    proyecto

---
+++ Migration/AccesoDatosSql.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Migration
+{
+    public static class AccesoDatosSql
+    {
+        public static void EjecutarSQL(string sql, string cadenaConexion)
+        {
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+
+            SqlCommand cmd = new SqlCommand();
+            try
+            {
+                
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = sql;
+                cmd.Connection = conexion;
+                cmd.CommandTimeout = 12000;
+                conexion.Open();
+                cmd.ExecuteNonQuery();
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            finally
+            {
+                conexion.Close();
+            }
+            
+        }
+    }
+}
---
+++ Migration/Entidades.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migration
+{
+    public class Entidades
+    {
+    }
+
+    public class MetaData
+    {
+        public string TableName { get; set; }
+        public string ColumnName { get; set; }
+        public string DataTypeValor { get; set; }
+    }
+
+    public class Tablas
+    {
+        public string TableName { get; set; }
+        public int Orden { get; set; }
+    }
+}
